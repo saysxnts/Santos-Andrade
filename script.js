dd79f3cde79c9b9e3bb753079dfeead78e03919b
saysxnts <guilherme_oliveirass@outlook.com>
2024-09-16 14:29:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const paragraphs = document.querySelectorAll(".interface");
+
+document.addEventListener("scroll", function(){
+    paragraphs.forEach((paragraph) => {
+        if(isInView(paragraph)){
+            paragraph.classList.add("interface--visible");
+        }
+    });
+});
+
+function isInView(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.bottom > 0 && 
+        rect.top < 
+            (window.innerHeight - 150 || document.documentElement.clientHeight - 150)
+    );
+}
+
+
